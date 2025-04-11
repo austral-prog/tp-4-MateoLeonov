8b85e23454167c472b84e33e351bd9ba207b3946
MateoLeonov <mjleonov@mail.austral.edu.ar>
2025-04-11 20:42:51 -0300

    Update line.py

--- line.py
+++ line.py
@@ -12,7 +12,7 @@ print(f"\tY = {A}X + {B}\n")
 print("Dados los siguientes puntos:")
 Y1 = A * X1 + B
 Y2 = A * X2 + B
-print(f"\t P1 ({X1}, {Y1}")
+print(f"\t P1 ({X1}, {Y1})")
 print(f"\t P2 ({X2}, {Y2})")
 dist = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
 print(f"\nLa distancia entre ellos es: {dist}")
