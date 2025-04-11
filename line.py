35dea9c57c118ea6afaab030b179c8599c3a3f75
MateoLeonov <mjleonov@mail.austral.edu.ar>
2025-04-11 11:06:38 -0300

    Update line.py

--- line.py
+++ line.py
@@ -7,7 +7,7 @@ print(f"El coeficiente A de su ecuación de la recta es: {A}")
 print(f"El coeficiente B de su ecuación de la recta es: {B}")
 print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
 print(f"El coeficiente X2 de su ecuación de la recta es: {X2}\n")
-print("Para la siguiente ecuación")
+print("Para la siguiente ecuación:")
 print(f"\tY = {A}X + {B}\n")
 print("Dados los siguientes puntos:")
 Y1 = A * X1 + B
