531803ebdf9198f7cc9958a08513bf7e40c51cae
MateoLeonov <mjleonov@mail.austral.edu.ar>
2025-04-11 21:18:00 -0300

    Update line.py

--- line.py
+++ line.py
@@ -7,11 +7,11 @@ def line():
     print(f"El coeficiente A de su ecuación de la recta es: {A}")
     print(f"El coeficiente B de su ecuación de la recta es: {B}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
-    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
-    print("")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}\n")
+    
     print("Para la siguiente ecuación:")
-    print(f"\tY = {A}X + {B}")
-    print("")    
+    print(f"\tY = {A}X + {B}\n")
+    
     print("Dados los siguientes puntos:")
     Y1 = A * X1 + B
     Y2 = A * X2 + B
@@ -19,5 +19,4 @@ def line():
     print(f"\tP2 ({X2}, {Y2})")
     
     dist = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
-    print("")
-    print(f"La distancia entre ellos es: {dist}")
+    print(f"\nLa distancia entre ellos es: {dist}")
